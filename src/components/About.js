b0de19a71b7e42a68fcd64aab767f9e544e78104
Arun Kumar <arun021997@gmail.com>
2021-03-27 12:32:08 +0530

    second edition of about

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,13 @@ import my_logo from "/Users/arunkrishnamoorthy/Downloads/My_Portfolio-navbar/src
 const About = () => {
     return (
 
-        <div className="container about">
+        <div className="container about shadow-lg padding-size  bg-body rounded-corners">
             <div className="row">
-                <div className="col-sm">
-                    <img src={my_logo} alt="My pic"></img>
+                <div className="col-sm ">
+                    <img className="my_logo" src={my_logo} alt="My pic"></img>
                 </div>
-                <div className="col-sm">
-                    <h1>Hi, I’m Arun Krishnamoorthy.</h1>
+                <div className="col-sm ">
+                    <h4>Hi, I’m Arun Krishnamoorthy.</h4>
                     <p>Indian,Engineer, Traveler, Photographer, Cinematographer, Coder. </p>
                     <p>Engaged with Travel, Camera, Technology, Meeting Unknown Peoples. Oueriyacity to learn something.</p>
                     <p>Seeking to be inspired, to envision the unlikely, to work hard for things that are worth it, and to be surrounded by those who bring out the best in me.</p>
